42175aa0d3d0a7fc02e3bf2c9131f64c641fe4fc
Ohgiraffers-bear <bear.ohgiraffers@gmail.com>
2024-12-18 14:43:21 +0900

    sout

--- src/com/ohgiraffers/example/Application.java
+++ src/com/ohgiraffers/example/Application.java
@@ -2,6 +2,8 @@ package com.ohgiraffers.example;
 
 public class Application {
     public static void main(String[] args) {
-
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
