ded1ad12798d8552739f1d59bbc548dccaabda34
monkey-ohgiraffers <monkey.ohgiraffers@gmail.com>
2024-12-18 14:49:14 +0900

    add source

--- src/com/ohgiraffers/example/Application.java
+++ src/com/ohgiraffers/example/Application.java
@@ -2,8 +2,14 @@ package com.ohgiraffers.example;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("hello world");
         System.out.println("Hello World");
         System.out.println("Hello World");
-        System.out.println("Hello World");
+
+    }
+
+    public void test(){
+        System.out.println("메소드 작성");
+
     }
 }
